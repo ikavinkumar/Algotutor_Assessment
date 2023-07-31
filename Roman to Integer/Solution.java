ad01c64bc3e688f1d8241f993c93fcbb6d2e5f22
kavinkumar2404 <87380595+kavinkumar2404@users.noreply.github.com>
2023-07-31 10:25:07 +0530

    added files

---
+++ Roman to Integer/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0, n = 0;
+        for (int index = s.length()-1; index >= 0; index--) {
+            switch(s.charAt(index)) {
+                case 'I': n = 1; break;
+                case 'V': n = 5; break;
+                case 'X': n = 10; break;
+                case 'L': n = 50; break;
+                case 'C': n = 100; break;
+                case 'D': n = 500; break;
+                case 'M': n = 1000; break;
+            }
+            if (4 * n < result) {
+                result -= n;
+            }
+            else{
+                 result += n;
+            }
+    }
+            return result;
+    }
+}
